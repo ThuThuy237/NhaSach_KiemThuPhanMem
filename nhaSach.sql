dcd20fc7bee66dc8758bb77edd85628d9091d8ba
thuthuy237 <thuthuy237@gmail.com>
2021-05-10 12:25:58 +0700

    database

--- nhaSach.sql
+++ nhaSach.sql
@@ -34,7 +34,7 @@ CREATE TABLE `books` (
   PRIMARY KEY (`id`),
   KEY `cat_id` (`cat_id`),
   CONSTRAINT `books_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `categories` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (1,'Mắt Biếc','Nguyễn Nhật Ánh',72,50000,62500,'images/mat-biec.png',4),(2,'Đại Học Không Lạc Hướng ','Lý Thượng Long',109,70000,87500,'images/dai-hoc-khong-lac-huong.png',4),(3,'Thoát Khỏi Tâm Trí ','Steven C.Hayes & Spencer Smith',459,100000,125000,'images/thoat-khoi-tam-tri-va-buoc-vao-cuoc-song_113991_1.jpg',4),(4,'Từ điển Oxford Anh – Việt','Oxford',1001,200000,250000,'images/tudienoxford.png',3),(5,'Từ điển Tiếng Việt Hoàng Phê','Viện Ngôn ngữ học',400,350000,437500,'images/aba183a8895f54eb546fb97d7fbfbbdc.jpg',3),(6,'Tập  Sách Thay Đổi Lịch Sử','Nhiều tác giả ',301,200000,250000,'images/bc9cc397e3b6e2fded3710d09cfb0478.jpg',5),(7,'Mắt Thiên Phật','Tạ Tấn',411,75000,93750,'images/mat-thien-phat.jpg',1),(8,'Doraemon  Tập 10 ','Fujiko Fujio',1000,10000,12500,'images/001.png',2),(9,'Doraemon Tập 8','Fujiko Fujio',996,10000,12500,'images/8_68.jpg',2),(10,'Me Before You: A Novel','Jojo Moyes',990,200000,250000,'images/anh-bia-me-before-you-e1580920303980.jpg',1),(11,'The Alchemist','Paulo Coelho',500,350000,437500,'images/71aFt4+OTOL.jpg',1),(12,'The Fault in Our Stars','John Green',300,150000,187500,'images/81yAo5ElQlL.jpg',1),(13,'A Bend in the Road','Nicholas Sparks',500,100000,125000,'images/201610-bend-in-the-road.jpg',1),(14,'At First Sight','Nicholas Sparks',495,150000,187500,'images/201610-at-first-sight.jpg',1),(15,'Doraemon Truyện Dài (Tập 5)','Fujiko Fujio',1000,100000,125000,'images/c.jpg',2),(16,'Doraemon Truyện Dài (Tập 1)','Fujiko Fujio',1000,100000,125000,'images/1_163.jpg',2),(17,'Doraemon Truyện Dài (Tập 2)','Fujiko Fujio',1000,100000,125000,'images/10.u5762.d20171018.t104146.65072.jpg',2),(18,'Doraemon Truyện Dài (Tập 3)','Fujiko Fujio',1000,100000,125000,'images/3_160.jpg',2),(19,'Từ Điển Việt - Hàn','Khang Việt',280,200000,250000,'images/sach-tu-dien-viet-han.jpg',3),(20,'Từ Điển Toán học & Tin Học Anh - Việt','Ban từ điển KHKT',500,200000,250000,'images/1.jpg',3),(21,'Thiên Thần Nhỏ Của Tôi ','Nguyễn Nhật Ánh',750,20000,25000,'images/thien-than-nho-cua-toi.jpg',1);
+INSERT INTO `books` VALUES (1,'Mắt Biếc','Nguyễn Nhật Ánh',72,50000,62500,'images/mat-biec.png',4),(2,'Đại Học Không Lạc Hướng ','Lý Thượng Long',109,70000,87500,'images/dai-hoc-khong-lac-huong.png',4),(3,'Thoát Khỏi Tâm Trí ','Steven C.Hayes & Spencer Smith',459,100000,125000,'images/thoat-khoi-tam-tri-va-buoc-vao-cuoc-song_113991_1.jpg',4),(4,'Từ điển Oxford Anh – Việt','Oxford',1001,200000,250000,'images/tudienoxford.png',3),(5,'Từ điển Tiếng Việt Hoàng Phê','Viện Ngôn ngữ học',400,350000,437500,'images/aba183a8895f54eb546fb97d7fbfbbdc.jpg',3),(6,'Tập  Sách Thay Đổi Lịch Sử','Nhiều tác giả ',301,200000,250000,'images/bc9cc397e3b6e2fded3710d09cfb0478.jpg',5),(7,'Mắt Thiên Phật','Tạ Tấn',411,75000,93750,'images/mat-thien-phat.jpg',1),(8,'Doraemon  Tập 10 ','Fujiko Fujio',1000,10000,12500,'images/001.png',2),(9,'Doraemon Tập 8','Fujiko Fujio',996,10000,12500,'images/8_68.jpg',2),(10,'Me Before You: A Novel','Jojo Moyes',990,200000,250000,'images/anh-bia-me-before-you-e1580920303980.jpg',1),(11,'The Alchemist','Paulo Coelho',500,350000,437500,'images/71aFt4+OTOL.jpg',1),(12,'The Fault in Our Stars','John Green',300,150000,187500,'images/81yAo5ElQlL.jpg',1),(13,'A Bend in the Road','Nicholas Sparks',500,100000,125000,'images/201610-bend-in-the-road.jpg',1),(14,'At First Sight','Nicholas Sparks',495,150000,187500,'images/201610-at-first-sight.jpg',1),(15,'Doraemon Truyện Dài (Tập 5)','Fujiko Fujio',1000,100000,125000,'images/c.jpg',2),(16,'Doraemon Truyện Dài (Tập 1)','Fujiko Fujio',1000,100000,125000,'images/1_163.jpg',2),(17,'Doraemon Truyện Dài (Tập 2)','Fujiko Fujio',1000,100000,125000,'images/10.u5762.d20171018.t104146.65072.jpg',2),(18,'Doraemon Truyện Dài (Tập 3)','Fujiko Fujio',1000,100000,125000,'images/3_160.jpg',2),(19,'Từ Điển Việt - Hàn','Khang Việt',280,200000,250000,'images/sach-tu-dien-viet-han.jpg',3),(20,'Từ Điển Toán học & Tin Học Anh - Việt','Ban từ điển KHKT',500,200000,250000,'images/1.jpg',3),(21,'Thiên Thần Nhỏ Của Tôi ','Nguyễn Nhật Ánh',750,20000,25000,'images/thien-than-nho-cua-toi.jpg',1),(23,'Trang Tí','unknow',300,20000,25000,'',2);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +65,7 @@ CREATE TABLE `buy` (
   KEY `emm_id` (`emm_id`),
   CONSTRAINT `buy_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`),
   CONSTRAINT `buy_ibfk_2` FOREIGN KEY (`emm_id`) REFERENCES `employee` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,7 +74,7 @@ CREATE TABLE `buy` (
 
 LOCK TABLES `buy` WRITE;
 /*!40000 ALTER TABLE `buy` DISABLE KEYS */;
-INSERT INTO `buy` VALUES (1,'2020-12-16 18:14:46',1050000,1,1),(2,'2020-12-16 18:14:46',2400000,2,1);
+INSERT INTO `buy` VALUES (1,'2020-12-16 18:14:46',1050000,1,1),(2,'2020-12-16 18:14:46',2400000,2,1),(7,'2021-05-09T21:14:00.597733500',2147480000,3,2),(8,'2021-05-09T21:15:29.932171600',2147480000,3,2),(9,'2021-05-09T21:17:33.987589900',50000000,2,2),(10,'2021-05-09T21:20:08.844038300',500000,2,2),(11,'2021-05-09T21:20:30.970102700',2147480000,4,2),(12,'2021-05-09T21:32:22.994220200',500000,4,2);
 /*!40000 ALTER TABLE `buy` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,7 +96,7 @@ CREATE TABLE `buy_detail` (
   KEY `buy_id` (`buy_id`),
   CONSTRAINT `buy_detail_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
   CONSTRAINT `buy_detail_ibfk_2` FOREIGN KEY (`buy_id`) REFERENCES `buy` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,7 +105,7 @@ CREATE TABLE `buy_detail` (
 
 LOCK TABLES `buy_detail` WRITE;
 /*!40000 ALTER TABLE `buy_detail` DISABLE KEYS */;
-INSERT INTO `buy_detail` VALUES (1,2,1,15,70000),(2,1,2,18,50000),(3,3,2,15,100000);
+INSERT INTO `buy_detail` VALUES (1,2,1,15,70000),(2,1,2,18,50000),(3,3,2,15,100000),(8,3,10,2,250000),(9,4,11,200,50000000),(10,3,12,2,250000);
 /*!40000 ALTER TABLE `buy_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -149,7 +149,7 @@ CREATE TABLE `customer` (
   `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `phone` varchar(15) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -158,7 +158,7 @@ CREATE TABLE `customer` (
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-INSERT INTO `customer` VALUES (1,'newCustomer','unknow','unknow','unknow','unkonw'),(2,'Khach 1','male','11/10/2000','200 Lê Thánh Tôn, P. Bến Thành, Quận 1, TP. HCM','0382367634'),(3,'Khach 2','female','15/09/2000','306 Nguyễn Trãi, P.8, Q.5','0372524817'),(4,'Khach 3','male','5/09/2001','412 Lê Văn Sỹ, P.14, Q.3','0338699707'),(5,'Khach 4','female','02/03/1985','433 Nơ Trang Long, P.13, Q. Bình Thạnh','0387742653'),(6,'Khach 5','male','12/12/1996','161A Nguyễn Văn Thủ, Q.1','0869578117');
+INSERT INTO `customer` VALUES (1,'myCustomer','male','1869-10-22','unknow','unkonw'),(2,'Khach 1','male','2000-10-11','200 Lê Thánh Tôn, P. Bến Thành, Quận 1, TP. HCM','0382367634'),(3,'Khach 2','female','2000-05-09','306 Nguyễn Trãi, P.8, Q.5','0372524817'),(4,'Khach 3','male','2001-11-23','412 Lê Văn Sỹ, P.14, Q.3','0338699707'),(5,'Khach 4','female','1985-05-30','433 Nơ Trang Long, P.13, Q. Bình Thạnh','0387742653'),(6,'Khach 5','male','1999-10-10','161A Nguyễn Văn Thủ, Q.1','0869578117'),(8,'thuthuy','Male','2000-09-25','Việt Nam','0941662403rhd');
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -212,7 +212,7 @@ CREATE TABLE `login` (
 
 LOCK TABLES `login` WRITE;
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` VALUES (1,'admin','827ccb0eea8a706c4c34a16891f84e7b','admin@gmail.com','/static/images/1.png','ADMIN'),(2,'thuy','3cf2b6b121d1726bc2cdc88c39e0b119','1851050140thuy@ou.edu.vn','/static/images/upload/15059724824858----nh-7.jpg','USER');
+INSERT INTO `login` VALUES (1,'admin','827ccb0eea8a706c4c34a16891f84e7b','admin@gmail.com','/src/main/resources/images/8025287921598811056-512.png','ADMIN'),(2,'thuy','3cf2b6b121d1726bc2cdc88c39e0b119','1851050140thuy@ou.edu.vn','/static/images/upload/15059724824858----nh-7.jpg','USER');
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -234,7 +234,7 @@ CREATE TABLE `order` (
   KEY `order_ibfk_1` (`emm_id`),
   CONSTRAINT `order_ibfk_1` FOREIGN KEY (`emm_id`) REFERENCES `employee` (`id`),
   CONSTRAINT `order_ibfk_2` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -243,7 +243,7 @@ CREATE TABLE `order` (
 
 LOCK TABLES `order` WRITE;
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
-INSERT INTO `order` VALUES (1,'2020-12-16 16:46:17',1906250,NULL,1),(2,'2020-12-16 16:46:43',1906250,NULL,1),(3,'2020-12-16 17:17:59',1968750,NULL,1),(5,'2020-12-16 21:26:38',625000,1,2),(14,'2020-12-16 21:31:31',4937500,1,2),(15,'2020-12-16 23:31:20',6250000,1,2),(16,'2020-12-17 00:25:23',325000,NULL,2);
+INSERT INTO `order` VALUES (1,'2021-05-09T21:32:34.336749300',9000000,2,4);
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -265,7 +265,7 @@ CREATE TABLE `order_detail` (
   KEY `order_id` (`order_id`),
   CONSTRAINT `order_detail_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
   CONSTRAINT `order_detail_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -274,7 +274,7 @@ CREATE TABLE `order_detail` (
 
 LOCK TABLES `order_detail` WRITE;
 /*!40000 ALTER TABLE `order_detail` DISABLE KEYS */;
-INSERT INTO `order_detail` VALUES (1,10,1,1,250000),(2,11,1,1,437500),(3,12,1,1,187500),(4,13,1,1,125000),(5,15,1,1,125000),(6,16,1,2,125000),(7,17,1,2,125000),(8,7,1,3,93750),(9,10,2,1,250000),(10,11,2,1,437500),(11,12,2,1,187500),(12,13,2,1,125000),(13,15,2,1,125000),(14,16,2,2,125000),(15,17,2,2,125000),(16,7,2,3,93750),(17,10,3,1,250000),(18,11,3,2,437500),(19,12,3,2,187500),(20,13,3,3,125000),(21,7,3,1,93750),(22,1,5,10,62500),(23,1,14,10,62500),(24,10,14,10,250000),(25,14,14,5,187500),(26,2,14,10,87500),(27,1,15,20,62500),(28,19,15,20,250000),(29,12,16,1,187500),(30,13,16,1,125000),(31,8,16,1,12500);
+INSERT INTO `order_detail` VALUES (1,4,1,6,1500000);
 /*!40000 ALTER TABLE `order_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -343,4 +343,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-15 20:20:17
+-- Dump completed on 2021-05-10 12:25:02
