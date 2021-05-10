12fe355e7071ada12db3355ce023321949a17e4d
KieuTrinh2709 <1851050155trinh@ou.edu.vn>
2021-05-10 16:23:28 +0700

    Login manage

--- NhaSach/src/main/java/com/mynhasach/service/LoginService.java
+++ NhaSach/src/main/java/com/mynhasach/service/LoginService.java
@@ -5,6 +5,7 @@
  */
 package com.mynhasach.service;
 
+import com.mynhasach.pojo.Buy;
 import com.mynhasach.pojo.Login;
 
 import java.sql.*;
@@ -101,4 +102,27 @@ public class LoginService {
         return preparedStatement.executeQuery().next();
     }
 
+    public boolean deleteLogin(int id) throws SQLException{
+        String sql = "DELETE FROM `nhasach`.`login` WHERE (`id` = ?);";
+        PreparedStatement preparedStatement = this.conn.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+
+        return preparedStatement.executeUpdate()>0;
+
+    }
+
+    public boolean updateLogin(Login login) throws SQLException{
+        String sql = "UPDATE `nhasach`.`login` SET `username` = ?, `password` = ?, `email` = ?, `avatar` = ?, `login_role` = ?  WHERE (`id` = ?);";
+        PreparedStatement preparedStatement = this.conn.prepareStatement(sql);
+        preparedStatement.setInt(6, login.getId());
+        preparedStatement.setString(1, login.getUsername());
+        preparedStatement.setString(2, login.getPassword());
+        preparedStatement.setString(3, login.getEmail());
+        preparedStatement.setString(4, login.getAvatar());
+        preparedStatement.setString(5,"USER");
+
+        return preparedStatement.executeUpdate()>0;
+
+
+    }
 }
